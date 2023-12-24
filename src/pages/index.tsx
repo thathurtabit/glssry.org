5539a6e3a04a84cdc52a32d773dc698672c9a868
Stephen Fairbanks <ghosthorses@outlook.com>
2023-12-24 13:45:55 +0000

    :sparkles: feat: add favions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,16 @@ import { PageMain } from "~/components/molecules/page-main/page-main";
 import { PostRowsLinks } from "~/components/molecules/post-rows-links/post-rows-links";
 import { SharedHead } from "~/components/molecules/shared-head/shared-head";
 import { useReadLatestPosts } from "~/hooks/post/read-latest-posts.hook";
-import { appDescription, appTitle } from "~/settings/constants";
+import { appDescription, appStrapline, appTitle } from "~/settings/constants";
 
 export default function Home() {
   const { latestPostsData, latestPostsDataIsFetching } = useReadLatestPosts();
   return (
     <>
-      <SharedHead title={`Welcome to ${appTitle}`} />
+      <SharedHead
+        title={`${appTitle}.org - ${appStrapline}`}
+        description={appDescription}
+      />
       <PageMain justifyContent="start">
         <PageMainIndent>
           <PageIntro textList={["Welcome", appDescription]} />
