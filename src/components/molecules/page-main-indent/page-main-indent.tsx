4e7bd639ad9c211f600612c759366d68704909e4
Stephen Fairbanks <ghosthorses@outlook.com>
2023-12-27 17:24:49 +0000

    :recycle: refactor(ui): add className prop to PageMainIndent

--- src/components/molecules/page-main-indent/page-main-indent.tsx
+++ src/components/molecules/page-main-indent/page-main-indent.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { FCC } from "~/types/react.types";
+import type { IPageMainIndent } from "./page-main-indent.types";
 
-export const PageMainIndent: FCC = ({ children }) => (
-  <div className="container max-w-2xl">{children}</div>
-);
+export const PageMainIndent: FCC<IPageMainIndent> = ({
+  children,
+  className = "max-w-2xl",
+}) => <div className={`container ${className}`}>{children}</div>;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { SectionSubtitle } from "~/components/atoms/section-subtitle/section-subtitle";
 import { CategoryRowsLinks } from "~/components/molecules/category-rows-links/category-rows-links";
 import { Intro } from "~/components/molecules/intro/intro";
+import { PageMainIndent } from "~/components/molecules/page-main-indent/page-main-indent";
 import { PageMain } from "~/components/molecules/page-main/page-main";
 import { PostRowsLinks } from "~/components/molecules/post-rows-links/post-rows-links";
 import { PostShort } from "~/components/molecules/post-short/post-short";
@@ -22,32 +23,34 @@ export default function Home() {
         description={appDescription}
       />
       <PageMain justifyContent="start">
-        <section className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 lg:gap-24 w-full justify-center items-center">
-          <Intro />
-          <PostShort
-            isLoading={randomPostDataIsFetching}
-            postData={randomPostData}
-            className="md:max-w-md"
-          />
-          <div className="flex flex-col items-start mb-10 md:mb-0 md:justify-self-end md:max-w-md w-full">
-            <SectionSubtitle className="mb-0">
-              Random categories
-            </SectionSubtitle>
-            <CategoryRowsLinks
-              isLoading={randomCategoryPostCountIsFetching}
-              categoryData={randomCategoryPostCountData}
-              itemsCount={10}
+        <PageMainIndent className="w-full max-w">
+          <section className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 lg:gap-24 w-full justify-center items-center">
+            <Intro />
+            <PostShort
+              isLoading={randomPostDataIsFetching}
+              postData={randomPostData}
+              className="md:max-w-md"
             />
-          </div>
-          <div className="md:max-w-md">
-            <SectionSubtitle className="mb-0">Latest posts</SectionSubtitle>
-            <PostRowsLinks
-              isLoading={latestPostsDataIsFetching}
-              postsData={latestPostsData}
-              itemsCount={10}
-            />
-          </div>
-        </section>
+            <div className="flex flex-col items-start mb-10 md:mb-0 md:justify-self-end md:max-w-md w-full">
+              <SectionSubtitle className="mb-0">
+                Random categories
+              </SectionSubtitle>
+              <CategoryRowsLinks
+                isLoading={randomCategoryPostCountIsFetching}
+                categoryData={randomCategoryPostCountData}
+                itemsCount={10}
+              />
+            </div>
+            <div className="md:max-w-md">
+              <SectionSubtitle className="mb-0">Latest posts</SectionSubtitle>
+              <PostRowsLinks
+                isLoading={latestPostsDataIsFetching}
+                postsData={latestPostsData}
+                itemsCount={10}
+              />
+            </div>
+          </section>
+        </PageMainIndent>
       </PageMain>
     </>
   );
