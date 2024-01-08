79205acc098fe07e8343fafb48496de7716a71e2
Stephen Fairbanks <stephen.fairbanks@autotrader.co.uk>
2024-01-08 10:00:30 +0000

    :lipstick: style(ui): remove x spacing on short post loading state

--- src/components/molecules/post-short-loading/post-short-loading.tsx
+++ src/components/molecules/post-short-loading/post-short-loading.tsx
@@ -4,7 +4,7 @@ import { postRowItemMetaStyles } from "~/styles/shared";
 
 export const PostShortLoading: FC = () => (
   <article className="relative">
-    <div className="py-2 px-4 mb-5 flex flex-col items-start justify-start gap-4 animate-pulse">
+    <div className="py-2 mb-5 flex flex-col items-start justify-start gap-4 animate-pulse">
       <div className="flex-1 flex flex-col gap-2 w-80">
         <span className="flex items-center gap-1 uppercase opacity-50 text-xs bg-loading h-9 w-64" />
         <span className="flex items-center gap-1 uppercase opacity-50 text-xs bg-loading h-9 w-56" />
